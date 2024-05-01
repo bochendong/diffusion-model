3f83a356a8460d03043ae44b8bb09fa5437da899
Bochen Dong <35963069+bochendong@users.noreply.github.com>
2024-05-01 04:52:10 -0400

    Update ddpm.py

--- DDPM/ddpm.py
+++ DDPM/ddpm.py
@@ -9,12 +9,54 @@ import torch
 from torch import optim, nn
 from torch.nn import functional as F
 from torch.utils import data
-from torchvision import datasets, transforms, utils, models
+from torchvision import datasets, transforms, utils
 from torchvision.transforms import functional as TF
 from tqdm.notebook import tqdm, trange
 from torchvision.utils import save_image
-import numpy as np
 
+def get_alphas_sigmas(t):
+    """
+    Returns the scaling factors for the clean image (alpha) and for the
+    noise (sigma), given a timestep.
+    """
+    return torch.cos(t * math.pi / 2), torch.sin(t * math.pi / 2)
+
+batch_size = 100
+epoches = 10
+
+# Actually train the model
+ema_decay = 0.999
+
+# The number of timesteps to use when sampling
+steps = 500
+
+# The amount of noise to add each timestep when sampling
+eta = 1.
+
+# Classifier-free guidance scale (0 is unconditional, 1 is conditional)
+guidance_scale = 2.
+
+
+tf = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize([0.5], [0.5]),
+])
+
+train_set = datasets.CIFAR10('data', train=True, download=True, transform=tf)
+train_dl = data.DataLoader(train_set, batch_size, shuffle=True,
+                           num_workers=4, persistent_workers=True, pin_memory=True)
+val_set = datasets.CIFAR10('data', train=False, download=True, transform=tf)
+val_dl = data.DataLoader(val_set, batch_size,
+                         num_workers=4, persistent_workers=True, pin_memory=True)
+
+if (os.path.exists("./output")) == False:
+    os.mkdir("output")
+
+files = glob.glob("./output/*.png")
+
+for f in files:
+    os.remove(f)
+     
 
 class ResidualBlock(nn.Module):
     def __init__(self, main, skip=None):
@@ -92,84 +134,8 @@ class Diffusion(nn.Module):
 
         self.timestep_embed = FourierFeatures(1, 16)
         self.class_embed = nn.Embedding(11, 4)
-        self.skip = nn.Identity()
-        self.block_1 = nn.Sequential(                   # 32x32
-            ResConvBlock(3 + 16 + 4, 64, 64),
-            ResConvBlock(64, 64, 64)
-        )
 
-        self.block_2 = nn.Sequential( 
-            nn.AvgPool2d(2),                            # 32x32 -> 16x16
-            ResConvBlock(64, 128, 128),
-            ResConvBlock(128, 128, 128)
-        )
-
-
-        self.block_3 = nn.Sequential( 
-            nn.AvgPool2d(2),                            # 16x16 -> 8x8
-            ResConvBlock(128, 256, 256),
-            SelfAttention2d(256, 256 // 64),
-            ResConvBlock(256, 256, 256),
-            SelfAttention2d(256, 256 // 64),
-        )
-
-        self.block_4 =  nn.Sequential( 
-            nn.AvgPool2d(2),                            # 8x8 -> 4x4
-            ResConvBlock(256, 512, 512),
-            SelfAttention2d(512, 512 // 64),
-            ResConvBlock(512, 512, 512),
-            SelfAttention2d(512, 512// 64),
-            ResConvBlock(512, 512, 512),
-            SelfAttention2d(512, 512 // 64),
-            ResConvBlock(512, 512, 256),
-            SelfAttention2d(256, 256// 64),
-            nn.Upsample(scale_factor = 2, mode = 'bilinear', align_corners = False),
-        )
-
-        self.block_5 = nn.Sequential(                   # 4x4 -> 8x8
-            ResConvBlock(512, 256, 256),
-            SelfAttention2d(256, 256 // 64),
-            ResConvBlock(256, 256, 128),
-            SelfAttention2d(128, 128// 64),
-        )
-
-        self.block_6 = nn.Sequential(                   # 8x8 -> 16x16
-            ResConvBlock(256, 128, 128),
-            ResConvBlock(128, 128, 64),
-            nn.Upsample(scale_factor = 2, mode='bilinear', align_corners=False),
-        )
-
-        self.block_7 = nn.Sequential(
-            ResConvBlock(128, 64, 64),
-            ResConvBlock(64, 64, 3, is_last=True)
-        )
-    def forward(self, input, t, cond):
-        timestep_embed = expand_to_planes(self.timestep_embed(t[:, None]), input.shape)
-        class_embed = expand_to_planes(self.class_embed(cond + 1), input.shape)
-
-        x = torch.cat([input, class_embed, timestep_embed], dim=1)
-
-        x_1 = self.block_1(x)
-
-        x_2 = self.block_2(x_1)
-
-        x_3 = self.block_3(x_2)
-
-        x_4 = self.block_4(x_4)
-        x_4 =  torch.cat([x_4, self.skip(x_3)], dim=1)
-
-        x_5 = self.block_5(x_4)
-        x_5 = torch.cat([x_5, self.skip(x_2)], dim=1)
-
-        x_6 = self.block_6(x_5)
-        x_6 = torch.cat([x_6, self.skip(x_1)], dim=1)
-
-        x_7 = self.block_7(x_6)
-
-        return x_7
-
-c = 64
-self.net = nn.Sequential(   # 32x32
+        self.net = nn.Sequential(   # 32x32
             ResConvBlock(3 + 16 + 4, c, c),
             ResConvBlock(c, c, c),
             SkipBlock([
@@ -207,3 +173,121 @@ self.net = nn.Sequential(   # 32x32
             ResConvBlock(c * 2, c, c),
             ResConvBlock(c, c, 3, is_last=True),
         )
+
+    def forward(self, input, t, cond):
+        timestep_embed = expand_to_planes(self.timestep_embed(t[:, None]), input.shape)
+        class_embed = expand_to_planes(self.class_embed(cond + 1), input.shape)
+        return self.net(torch.cat([input, class_embed, timestep_embed], dim=1))
+    
+seed = 0
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
+torch.manual_seed(0)
+
+model = Diffusion().to(device)
+model_ema = deepcopy(model)
+
+opt = optim.Adam(model.parameters(), lr=2e-4)
+scaler = torch.cuda.amp.GradScaler()
+
+# Use a low discrepancy quasi-random sequence to sample uniformly distributed
+# timesteps. This considerably reduces the between-batch variance of the loss.
+rng = torch.quasirandom.SobolEngine(1, scramble=True)
+
+@torch.no_grad()
+def sample(model, x, steps, eta, classes, guidance_scale=1.):
+    """
+    Draws samples from a model given starting noise.
+    """
+    ts = x.new_ones([x.shape[0]])
+
+    # Create the noise schedule
+    t = torch.linspace(1, 0, steps + 1)[:-1]
+    alphas, sigmas = get_alphas_sigmas(t)
+
+    # The sampling loop
+    for i in trange(steps):
+
+        # Get the model output (v, the predicted velocity)
+        with torch.cuda.amp.autocast():
+            x_in = torch.cat([x, x])
+            ts_in = torch.cat([ts, ts])
+            classes_in = torch.cat([-torch.ones_like(classes), classes])
+            v_uncond, v_cond = model(x_in, ts_in * t[i], classes_in).float().chunk(2)
+        v = v_uncond + guidance_scale * (v_cond - v_uncond)
+
+        # Predict the noise and the denoised image
+        pred = x * alphas[i] - v * sigmas[i]
+        eps = x * sigmas[i] + v * alphas[i]
+
+        # If we are not on the last timestep, compute the noisy image for the
+        # next timestep.
+        if i < steps - 1:
+            # If eta > 0, adjust the scaling factor for the predicted noise
+            # downward according to the amount of additional noise to add
+            ddim_sigma = eta * (sigmas[i + 1]**2 / sigmas[i]**2).sqrt() * \
+                (1 - alphas[i]**2 / alphas[i + 1]**2).sqrt()
+            adjusted_sigma = (sigmas[i + 1]**2 - ddim_sigma**2).sqrt()
+
+            # Recombine the predicted noise and predicted denoised image in the
+            # correct proportions for the next step
+            x = pred * alphas[i + 1] + eps * adjusted_sigma
+
+            # Add the correct amount of fresh noise
+            if eta:
+                x += torch.randn_like(x) * ddim_sigma
+
+    # If we are on the last timestep, output the denoised image
+    return pred
+
+def train(epoch):
+    for i, (reals, classes) in enumerate(tqdm(train_dl)):
+        opt.zero_grad()
+        reals = reals.to(device)
+        classes = classes.to(device)
+
+        # Draw uniformly distributed continuous timesteps
+        t = rng.draw(reals.shape[0])[:, 0].to(device)
+
+        # Calculate the noise schedule parameters for those timesteps
+        alphas, sigmas = get_alphas_sigmas(t)
+
+        # Combine the ground truth images and the noise
+        alphas = alphas[:, None, None, None]
+        sigmas = sigmas[:, None, None, None]
+        noise = torch.randn_like(reals)
+
+        # with t increase, alpha goes down and sigmas goes up
+        # which means with t increase, noised_reals will have more noise and less image
+        noised_reals = reals * alphas + noise * sigmas
+
+        # with t increase, alpha goes down and sigmas goes up
+        # which means with t increase, targets will closer to - img
+        targets = noise * alphas - reals * sigmas
+
+        # Drop out the class on 20% of the examples
+        to_drop = torch.rand(classes.shape, device=classes.device).le(0.2)
+        classes_drop = torch.where(to_drop, -torch.ones_like(classes), classes)
+
+        # Compute the model output and the loss.
+        v = model(noised_reals, t, classes_drop)
+        loss = F.mse_loss(v, targets)
+
+        # Do the optimizer step and EMA update
+        scaler.scale(loss).backward()
+        scaler.step(opt)
+
+        # ema_update(model, model_ema, 0.95 if epoch < 20 else ema_decay)
+        scaler.update()
+
+    print(f"Epoch {epoch+1}:")
+    noise = torch.randn([10, 3, 32, 32], device=device)
+    fakes_classes = torch.arange(10, device=device)
+    fakes = sample(model, noise, steps, eta, fakes_classes, guidance_scale)
+    fakes = (fakes + 1) / 2
+    fakes = torch.clamp(fakes, min=0, max = 1)
+    save_image(fakes.data, './output/%03d_train.png' % epoch)
+
+for epoch in range(epoches):
+    train(epoch)
